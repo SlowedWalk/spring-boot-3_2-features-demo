1440f22c2aba622c893335c6b32089c7c5e8d4f5
Hidetora <idetoratojo1@gmail.com>
2024-01-25 11:21:43 +0100

    starting native compile

--- src/main/java/tech/hidetora/springboot3_2demo/Application.java
+++ src/main/java/tech/hidetora/springboot3_2demo/Application.java
@@ -1,17 +1,15 @@
 package tech.hidetora.springboot3_2demo;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.ai.openai.OpenAiChatClient;
-import org.springframework.ai.prompt.Prompt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Collection;
-import java.util.Map;
 
 @SpringBootApplication
 public class Application {
@@ -44,19 +42,18 @@ class CustomerHttpController {
     }
 }
 
-@Controller
-@ResponseBody
-@RequiredArgsConstructor
-class AiController {
-    private final OpenAiChatClient openAiChatClient;
-
-    // curl localhost:8080/customers
-    @GetMapping("/story")
-    public Map<String, String> send() {
-        var prompt = "tell a story about the the delicious food in Belgium, and do it in the voice of a 19th century French chef";
-        return Map.of("response", openAiChatClient.generate(prompt));
-    }
-}
+//@Controller
+//@ResponseBody
+//@RequiredArgsConstructor
+//class AiController {
+//    private final ChatClient chatClient;
+//
+//    // curl localhost:8080/customers
+//    @GetMapping("/ai/generate")
+//    public Map<String, String> generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+//        return Map.of("response", chatClient.generate(message));
+//    }
+//}
 
 interface CustomerRepository extends ListCrudRepository<Customer, Integer> {
     Collection<Customer> findByName(String name);
